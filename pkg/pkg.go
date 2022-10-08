7a2910e08fc4a9f00f6d5dc89bf39be8767e5a1e
klauskems <klauskems@gmail.com>
2022-10-08 11:29:16 +0900

    dei-2

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,10 +1,23 @@
 package pkg
 
+// slice 요소 삭제 s는 slice를 받고 a는 지우고 싶은 index
 func Dei(s []int, a int) []int {
 	l := len(s)
-	slice := s[:a]
-	for i := a + 1; i < l; i++ {
-		slice = append(slice, s[i])
-	}
-	return slice
+	copy(s[a:], s[a+1:])
+	s[l] = 0
+	return s
 }
+
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/klauskems/package/pkg"
+// )
+
+// func main() {
+// 	slice := []int{1, 2, 3, 4, 5, 6}
+// 	slice = pkg.Dei(slice, 3)
+// 	fmt.Println(slice)
+// }
