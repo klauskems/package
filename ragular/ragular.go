d49d8bdf8dea0d3098d1eb7ee759d3b77a360ced
klauskems <klauskems@naver.com>
2022-10-09 14:47:55 +0900

    koreapost

--- ragular/ragular.go
+++ ragular/ragular.go
@@ -9,9 +9,9 @@ func (f *FedexSender) Send(parcel string) {
 	fmt.Printf("Fedex sends %v parcel \n", parcel)
 }
 
-type koreaPost struct {
+type KoreaPost struct {
 }
 
-func (k *koreaPost) PSend(parcel string) {
+func (k *KoreaPost) PSend(parcel string) {
 	fmt.Printf("우체국에서 택배 %v를 보냅니다. \n", parcel)
 }
